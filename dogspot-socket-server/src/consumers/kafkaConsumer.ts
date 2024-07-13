6dd93190a966d9a61126d99b6102e8ea2c378d17
문준호 <wngh46280@gmail.com>
2024-07-13 23:39:13 +0900

    fix: 코드 수정

--- dogspot-socket-server/src/consumers/kafkaConsumer.ts
+++ dogspot-socket-server/src/consumers/kafkaConsumer.ts
@@ -1,16 +1,13 @@
 import kafka from '../core/config/kafkaConfig';
 import { Topic } from './helpers/constants';
 import { sendPushToUser } from '../modules/push.module'; // sendPushToUser 함수 import
-
-const topic = 'walks-push';
-const groupId = 'test-group';
-
+ 
+const groupId = process.env.KAFKA_GROUP_ID || 'default-group-id';
 const consumer = kafka.consumer({ groupId });
 
 export const runConsumer = async () => {
-  console.log('Consumer is starting...');
   await consumer.connect();
-  await consumer.subscribe({ topic });
+  await consumer.subscribe({ topic: Topic.WALKS_PUSH });
   console.log('Consumer is connected and subscribed.');
 
   await consumer.run({
@@ -20,9 +17,12 @@ export const runConsumer = async () => {
         return;
       }
       const value = message.value.toString(); // Kafka 메시지의 값 (JSON 문자열)
-      const data = JSON.parse(value); // JSON 문자열을 객체로 파싱
-      // 여기에 메시지 처리 로직을 추가합니다.
-      console.log("The topic is walks_push.: ", data);
+      let data;
+      try {
+        data = JSON.parse(value); // JSON 문자열을 객체로 파싱
+      } catch (error) {
+        throw error;
+      }
       // 메시지 처리가 완료되면 오프셋을 커밋합니다.
       switch (topic) {
         case Topic.WALKS_PUSH:
