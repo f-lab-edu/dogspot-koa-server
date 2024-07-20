1b7d5e6dacb2f3302d54eafac450b53c4f0f3e5a
문준호 <wngh46280@gmail.com>
2024-07-21 00:58:59 +0900

    fix: 미디어 서버 큐 제한 오류 수정3

--- dogspot-media-server/src/kafka/consumer.ts
+++ dogspot-media-server/src/kafka/consumer.ts
@@ -2,13 +2,37 @@ import kafka from '../core/config/kafkaConfig';
 import { walksControllerInstance } from '../domains/walks/walks.controller';
 import { Topic } from './helpers/constants';
 import { EachMessagePayload } from 'kafkajs';
-import pLimit from 'p-limit';  // ES 모듈로부터 직접 import
+
+class Semaphore {
+  private max: number;
+  private current: number;
+  private queue: Array<() => void> = [];
+
+  constructor(max: number) {
+    this.max = max;
+    this.current = 0;
+  }
+
+  async acquire() {
+    if (this.current >= this.max) {
+      await new Promise<void>((resolve) => this.queue.push(resolve));
+    }
+    this.current++;
+  }
+
+  release() {
+    this.current--;
+    if (this.queue.length > 0) {
+      const resolve = this.queue.shift();
+      if (resolve) resolve();
+    }
+  }
+}
+
+const semaphore = new Semaphore(5); // 동시에 최대 5개의 메시지를 처리하도록 설정
 
 const groupId = process.env.KAFKA_GROUP_ID || 'default-group-id';
 const consumer = kafka.consumer({ groupId });
-
-// 동시에 최대 5개의 메시지를 처리하도록 제한
-const limit = pLimit(5);
 
 export const runConsumer = async () => {
   await consumer.connect();
@@ -17,14 +41,14 @@ export const runConsumer = async () => {
   await consumer.run({
     eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
       try {
-        // p-limit을 사용하여 동시 실행 수를 제한합니다.
-        await limit(async () => {
+        await semaphore.acquire();  // Semaphore를 사용하여 동시 실행 수를 제한합니다.
+        try {
           if (message.value === null) {
             console.warn('Received a message with null value');
             return;
           }
-          const value = message.value.toString();  // 여기서 message.value는 null이 아님을 보장합니다.
-          const data = JSON.parse(value);  // JSON 파싱
+          const value = message.value.toString(); // 여기서 message.value는 null이 아님을 보장합니다.
+          const data = JSON.parse(value); // JSON 파싱
 
           let result;
 
@@ -41,8 +65,9 @@ export const runConsumer = async () => {
           if (result) {
             await consumer.commitOffsets([{ topic, partition, offset: (BigInt(message.offset) + BigInt(1)).toString() }]);
           }
-        });
-
+        } finally {
+          semaphore.release(); // Semaphore 해제
+        }
       } catch (error) {
         console.error(`Error in runConsumer eachMessage: ${error}`);
       }
